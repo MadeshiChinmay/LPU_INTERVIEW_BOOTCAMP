02120469e8892b000fa2b5fd0a67fa5c468a73d9
MadeshiChinmay <42076125+MadeshiChinmay@users.noreply.github.com>
2020-08-13 14:03:31 +0530

    Create DAY2_9.cpp

---
+++ DAY2_9.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int K) {
+        int N = points.length;
+        int[] dists = new int[N];
+        for (int i = 0; i < N; ++i)
+            dists[i] = dist(points[i]);
+
+        Arrays.sort(dists);
+        int distK = dists[K-1];
+
+        int[][] ans = new int[K][2];
+        int t = 0;
+        for (int i = 0; i < N; ++i)
+            if (dist(points[i]) <= distK)
+                ans[t++] = points[i];
+        return ans;
+    }
+
+    public int dist(int[] point) {
+        return point[0] * point[0] + point[1] * point[1];
+    }
+}
